a8e4f3d5ae80c15280f72ca765a945b9496e5da5
ujkhm <xl5071888@gmail.com>
2026-06-01 21:34:47 +0800

    新增簡單測試代碼

--- VSCode/esp32_code/src/main.cpp
+++ VSCode/esp32_code/src/main.cpp
@@ -1,14 +1,22 @@
 #include <Arduino.h>
+#include "HardwareSerial.h"
 #include "speed_sensor/speed_sensor.h"
+
+
+uint32_t Serial_time;
 
 void setup() {
   Serial.begin(115200);
   speed_sensor_init();
+  Serial_time = millis();
 }
 
 
 
 void loop() {
-  Serial.println("Hello, ESP32!");
-  delay(1000);
+  speed_sensor_start();
+  if(millis() - Serial_time >= 1000){
+    Serial.println(settings.now_speed);
+    Serial_time = millis();
+  }
 }
